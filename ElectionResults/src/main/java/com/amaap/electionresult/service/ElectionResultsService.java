6b217d0e3f3c00822e4a622c812f6190b208788a
Danake Pratiksha <danakepratiksha1020@gmail.com>
2024-05-29 14:43:02 +0530

    added outer service to use domain service

---
+++ ElectionResults/src/main/java/com/amaap/electionresult/service/ElectionResultsService.java
@@ -0,0 +1,35 @@
+package com.amaap.electionresult.service;
+
+import com.amaap.electionresult.domain.model.entity.Constituency;
+import com.amaap.electionresult.domain.model.entity.exception.InvalidConstituencyNameException;
+import com.amaap.electionresult.domain.model.entity.exception.InvalidPartyNameException;
+import com.amaap.electionresult.domain.service.WinnerEvaluatorService;
+import com.amaap.electionresult.domain.service.dto.WinnerPartyDto;
+import com.amaap.electionresult.service.io.DataReader;
+import com.amaap.electionresult.service.io.exception.InvalidFilePathException;
+import com.amaap.electionresult.service.io.exception.UnformattedInputLineException;
+import com.google.inject.Inject;
+
+import java.util.List;
+
+public class ElectionResultsService {
+    private DataReader dataReader;
+    private ConstituencyService constituencyService;
+    private WinnerEvaluatorService winnerEvaluatorService;
+    private WinnerDetailsPrintingService winnerDetailsPrintingService;
+
+    @Inject
+    public ElectionResultsService(DataReader dataReader, ConstituencyService constituencyService, WinnerEvaluatorService winnerEvaluatorService, WinnerDetailsPrintingService winnerDetailsPrintingService) {
+        this.dataReader = dataReader;
+        this.constituencyService = constituencyService;
+        this.winnerEvaluatorService = winnerEvaluatorService;
+        this.winnerDetailsPrintingService = winnerDetailsPrintingService;
+    }
+
+    public void evaluateElectionResults(String filePath) throws InvalidConstituencyNameException, InvalidFilePathException, InvalidPartyNameException, UnformattedInputLineException {
+        dataReader.readFile(filePath);
+        List<Constituency> constituenciesData = constituencyService.getConstituenciesData();
+        List<WinnerPartyDto> winners = winnerEvaluatorService.findWinner(constituenciesData);
+        winnerDetailsPrintingService.print(winners);
+    }
+}
---
+++ ElectionResults/src/test/java/com/amaap/electionresult/service/ElectionResultsServiceTest.java
@@ -0,0 +1,38 @@
+package com.amaap.electionresult.service;
+
+import com.amaap.electionresult.AppModule;
+import com.amaap.electionresult.domain.model.entity.exception.InvalidConstituencyNameException;
+import com.amaap.electionresult.domain.model.entity.exception.InvalidPartyNameException;
+import com.amaap.electionresult.domain.model.entity.exception.InvalidVoteCountException;
+import com.amaap.electionresult.service.io.exception.InvalidFilePathException;
+import com.amaap.electionresult.service.io.exception.UnformattedInputLineException;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ElectionResultsServiceTest {
+    private ElectionResultsService electionResultsService;
+
+    @BeforeEach
+    void initialize() {
+        Injector injector = Guice.createInjector(new AppModule());
+        electionResultsService = injector.getInstance(ElectionResultsService.class);
+    }
+
+    @Test
+    void shouldBeAbleToGetWinnersOfEachConstituencyByUsingDomainService() throws InvalidPartyNameException, InvalidVoteCountException, InvalidConstituencyNameException, FileNotFoundException, InvalidFilePathException, UnformattedInputLineException {
+        // arrange
+        String filePath = "E:\\ElectionResults\\ElectionResults\\src\\test\\java\\com\\amaap\\electionresult\\resource\\ResultData";
+
+        // act
+        electionResultsService.evaluateElectionResults(filePath);
+
+        // assert
+        assertTrue(true);
+    }
+}
