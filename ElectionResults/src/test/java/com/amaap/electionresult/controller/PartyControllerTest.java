687b34bd76203c347194c3a2be14a34a9d8bf8a2
Danake Pratiksha <danakepratiksha1020@gmail.com>
2024-05-30 13:49:44 +0530

    refactored test cases of PartyController

--- ElectionResults/src/test/java/com/amaap/electionresult/controller/PartyControllerTest.java
+++ ElectionResults/src/test/java/com/amaap/electionresult/controller/PartyControllerTest.java
@@ -36,11 +36,25 @@ public class PartyControllerTest {
     }
 
     @Test
-    void shouldBeAbleToReturnResponseAsBadRequestIfFailsToCreatesParty() throws FileNotFoundException {
+    void shouldBeAbleToReturnResponseAsBadRequestIfPartyNameProvidedIsInvalid() throws FileNotFoundException {
         // arrange
         Response expected = new Response(HttpStatus.BAD_REQUEST, "Invalid Data Provided");
         String partyName = "";
         int voteCount = 100;
+
+        // act
+        Response actual = partyController.createParty(partyName, voteCount);
+
+        // assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldBeAbleToReturnResponseAsBadRequestIfVoteCountProvidedIsInvalid() throws FileNotFoundException {
+        // arrange
+        Response expected = new Response(HttpStatus.BAD_REQUEST, "Invalid Data Provided");
+        String partyName = "BJP";
+        int voteCount = -111;
 
         // act
         Response actual = partyController.createParty(partyName, voteCount);
